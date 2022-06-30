4a5f0423c2a91e35cfd3fa1b88e04caffbb916c7
Roberto Tun <rtun2004@gmail.com>
2022-06-30 15:44:50 -0600

    Update TicTacToe.py

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,8 +1,7 @@
-from ast import Num
-
-
+Gameover = False
 board = ["-","-","-","-","-","-","-","-","-"] #list to input the x and o
-Nums = []
+Taken_Positions = []
+CurrentPlayer = "X"
 
 def display_board(board): #function that displays the board 
     print(board[0] + " | " + board[1] + " | " + board[2])
@@ -12,17 +11,41 @@ def display_board(board): #function that displays the board
     print(board[6] + " | " + board[7] + " | " + board[8])
 
 
-def check_input(x, p): #function that places the 'X' or 'O' at the place the user desires 
-    if p == "PlayerOne": #if that checks which player is inputting
+def input_position(x, p): #function that places the 'X' or 'O' at the place the user desires 
+    if p == "X": #if that checks which player is inputting
         for i in range(1, len(board)+1): #for that start at 1 and ends at the length of the list and adds 1
             if x == i: #that checks the input of the user
                 z = i - 1 #subtracting one from i because i is ahead by one of the index of the board list
                 board[z] = "X" #using z to change the index of the board list depending on the value of z
-    elif p == "PlayerTwo": #Does same thing as above
+    elif p == "O": #Does same thing as above
         for i in range(1, len(board)+1):
             if x == i:
                 z = i - 1
                 board[z] = "O"
+
+def CheckWinner():
+    if board[0] == "X" and board[1] == "X" and board[2] == "X":
+        Gameover = True
+        return Gameover
+    elif board[3] =="X" and board[4] == "X" and board[5] == "X":
+        Gameover = True
+        return Gameover
+    elif board[6] =="X" and board[7] == "X" and board[8] == "X":
+        Gameover = True
+        return Gameover
+    elif board[0] =="X" and board[3] == "X" and board[6] == "X":
+        Gameover = True
+        return Gameover
+    elif board[1] =="X" and board[4] == "X" and board[7] == "X":
+        Gameover = True
+        return Gameover
+    elif board[2] =="X" and board[5] == "X" and board[8] == "X":
+        Gameover = True
+        return Gameover
+
+
+
+
 
 def TicTacToe():
 
@@ -37,31 +60,31 @@ def TicTacToe():
     print("7 | 8 | 9")
     print("\n")
 
-    Gameover = False
-    Switch_Player = False
+    CurrentPlayer = "X"
     while Gameover == False:
-        while Switch_Player == False:
+        if CurrentPlayer == "X":
             print("X |Player's turn")
-            Xuser = int(input("Enter a number between 1 - 9: ")) #input needs to be a int to compare 
-            Player = "PlayerOne"
-            if Xuser in Nums:
+            Position = int(input("Enter a number between 1 - 9: ")) #input needs to be a int to compare 
+            if Position in Taken_Positions:
                 print("This Position is already taken")
             else:
-                check_input(Xuser, Player)
-                Nums.append(Xuser)
+                input_position(Position, CurrentPlayer)
+                Taken_Positions.append(Position)
                 display_board(board)
-                Switch_Player = True
+                CurrentPlayer = "O"
         else:
             print("O |Player's turn")
-            Xuser = int(input("Enter a number between 1 - 9: ")) #input needs to be a int to compare 
-            Player = "PlayerTwo"
-            if Xuser in Nums:
+            Position = int(input("Enter a number between 1 - 9: ")) #input needs to be a int to compare 
+            if Position in Taken_Positions:
                 print("This Position is already taken")
             else:
-                check_input(Xuser, Player)
-                Nums.append(Xuser)
-                display_board(board)
-                Switch_Player = False
+                if Position >= 1 and Position <= 9:
+                    input_position(Position, CurrentPlayer)
+                    Taken_Positions.append(Position)
+                    display_board(board)
+                    CurrentPlayer = "X"
+
+            
 
 
 
